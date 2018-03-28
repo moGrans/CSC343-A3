3351e6fade9d198739a8c27d6ad4e5639e04794d
Pan Zhiyang <panzhi2@teach.cs.toronto.edu>
2018-03-27 23:17:08 -0400

    q3 complete

--- q3.sql
+++ q3.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS q3 CASCADE;
 
 -- Final relation for answer 
 create table q3(
-name VARCHAR(50),
-model_number INT
+model_name VARCHAR(50),
+num_rented INT
 );
 
 -- Define views for your intermediate steps here.
@@ -14,13 +14,45 @@ model_number INT
 --------------------------------------------------------------------------
 -- idea for this question:
 --		1. Find reservation fully completed in 2017 (status = completed)
---		2. Filter car's rental stationlocation to Toronto
---      3. 
---      4. 	
+--		2. Find all car model that car's rental station location to Toronto
+--      3. Find most frequently rented car model
+--------------------------------------------------------------------------
+-- Find reservation fully completed in 2017 (status = completed)
+DROP VIEW IF EXISTS valid_resv CASCADE;
+CREATE VIEW valid_resv AS
+    SELECT id AS reservation_id, car_id
+    FROM Reservation
+    WHERE status = 'Completed' AND from_date > TIMESTAMP '2017-01-01 00:00:00' AND end_date < TIMESTAMP '2017-12-31-23:59:59';
+
+--------------------------------------------------------------------------
+-- Filter car's rental station location to Toronto
+DROP VIEW IF EXISTS yyz_resv CASCADE;
+CREATE VIEW yyz_resv AS
+    SELECT c.id, c.model_id, vr.reservation_id
+    FROM valid_resv vr JOIN Car c
+            ON vr.car_id = c.id
+        JOIN Rentalstation r 
+            ON c.station_code = r.station_code
+    WHERE r.city = 'Toronto';
+
+--------------------------------------------------------------------------
+-- Find most frequently rented car model
+DROP VIEW IF EXISTS yyz_model CASCADE;
+CREATE VIEW yyz_model AS
+    SELECT model_id, MAX(COUNT(reservation_id)) AS num_rent
+    FROM yyz_resv
+    GROUP BY model_id;
+
+--------------------------------------------------------------------------
+----------------------- End of Intermediate Steps ------------------------
 --------------------------------------------------------------------------
 
-
-
+-- the answer to the query 
+INSERT INTO q3
+	SELECT m.name AS model_name, yyzm.num_rent AS num_rented
+	FROM yyz_model yyzm JOIN Model m
+            ON yyzm.model_id = m.model_id
+    ORDER BY yyzm.num_rent [DESC], m.name;
 
 
 
