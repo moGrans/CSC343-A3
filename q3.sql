2a717f7173bd8ae5726d615ab317d7e12e722937
Pan Zhiyang <panzhi2@teach.cs.toronto.edu>
2018-03-28 22:14:27 -0400

    q3 bug fixed

--- q3.sql
+++ q3.sql
@@ -22,7 +22,7 @@ DROP VIEW IF EXISTS valid_resv CASCADE;
 CREATE VIEW valid_resv AS
     SELECT id AS reservation_id, car_id
     FROM Reservation
-    WHERE status = 'Completed' AND from_date > TIMESTAMP '2017-01-01 00:00:00' AND end_date < TIMESTAMP '2017-12-31-23:59:59';
+    WHERE status = 'Completed' AND from_date > TIMESTAMP '2017-01-01 00:00:00' AND end_date < TIMESTAMP '2017-12-31 23:59:59';
 
 --------------------------------------------------------------------------
 -- Filter car's rental station location to Toronto
@@ -36,20 +36,26 @@ CREATE VIEW yyz_resv AS
     WHERE r.city = 'Toronto';
 
 --------------------------------------------------------------------------
--- Find most frequently rented car model
+-- Find frequency of rented car model
 DROP VIEW IF EXISTS yyz_model CASCADE;
 CREATE VIEW yyz_model AS
-    SELECT model_id, MAX(COUNT(reservation_id)) AS num_rent
+    SELECT model_id, COUNT(reservation_id) AS num_rent
     FROM yyz_resv
     GROUP BY model_id;
 
+DROP VIEW IF EXISTS popular_model CASCADE;
+CREATE VIEW popular_model AS
+    SELECT yyz.model_id, yyz.num_rent
+    FROM yyz_model yyz JOIN (SELECT max(num_rent) AS most_popular FROM yyz_model) popular
+            ON yyz.num_rent = popular.most_popular;
 --------------------------------------------------------------------------
 ----------------------- End of Intermediate Steps ------------------------
 --------------------------------------------------------------------------
 
 -- the answer to the query 
 INSERT INTO q3
-	SELECT m.name AS model_name, yyzm.num_rent AS num_rented
-	FROM yyz_model yyzm JOIN Model m
-            ON yyzm.model_id = m.model_id
-    ORDER BY yyzm.num_rent [DESC], m.name;
+	SELECT m.name AS model_name, pm.num_rent AS num_rented
+	FROM popular_model pm JOIN Model m
+            ON pm.model_id = m.id
+    ORDER BY pm.num_rent DESC, m.name
+    LIMIT 1;
