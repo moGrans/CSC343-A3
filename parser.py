c5eb605fa6aad41c7ee63885a768231e1ec801e4
Pan Zhiyang <panzhi2@teach.cs.toronto.edu>
2018-03-28 22:20:28 -0400

    parser editted according to ddl

--- parser.py
+++ parser.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 def theSpliter(roughList):
     categoryCol = {}
@@ -38,7 +37,7 @@ def theSpliter(roughList):
 if __name__ == "__main__":
     result = {}
 
-    with open("Car-data (1).txt","r") as fd:
+    with open("car-data.txt","r") as fd:
         lines = fd.readlines()
 
         result = theSpliter(roughList=lines)
@@ -46,84 +45,19 @@ if __name__ == "__main__":
     with open("data.sql", "w") as sql:
         # Insert into Customer
         for element in result['Customer']:
-            sql.write("INSERT INTO Customer VALUES ('{}','{}','{}')\n".format(element["Name"], element["Age"], element["Email"]))
+            sql.write("INSERT INTO Customer VALUES ('{}',{},'{}');\n".format(element["Name"], element["Age"], element["Email"]))
         sql.write('\n')
         for element in result['Model']:
-            sql.write("INSERT INTO Model VALUES ('{}','{}','{}','{}','{}')\n".format(element["ID"], element["Name"], element["Vehicle Type"], element["Model Number"], element["Capacity (Seats)"]))
+            sql.write("INSERT INTO Model VALUES ({},'{}','{}',{},{});\n".format(element["ID"], element["Name"], element["Vehicle Type"], element["Model Number"], element["Capacity (Seats)"]))
         sql.write('\n')
         for element in result['RENTALSTATION']:
-            sql.write("INSERT INTO Rentalstation VALUES ('{}','{}','{}','{}','{}')\n".format(element["Station Code"], element["Name"], element["Address"], element["Area Code"], element["City"]))
+            sql.write("INSERT INTO Rentalstation VALUES ({},'{}','{}','{}','{}');\n".format(element["Station Code"], element["Name"], element["Address"], element["Area Code"], element["City"]))
         sql.write('\n')
         for element in result['CAR']:
-            sql.write("INSERT INTO Car VALUES ('{}','{}','{}','{}')\n".format(element["ID"], element["License Plate Number"], element["Station Code"], element["Model_Id"]))
+            sql.write("INSERT INTO Car VALUES ({},'{}',{},{});\n".format(element["ID"], element["License Plate Number"], element["Station Code"], element["Model_Id"]))
         sql.write('\n')
         for element in result['RESERVATION']:
-            sql.write("INSERT INTO Reservation VALUES ('{}','{}','{}','{}','{}','{}')\n".format(element["ID"], element["From Date"], element["To Date"], element["Car ID"], element["Old Reservation ID"], element["Status"]))
+            sql.write("INSERT INTO Reservation VALUES ({},'{}','{}',{},{},'{}');\n".format(element["ID"], element["From Date"], element["To Date"], element["Car ID"], element["Old Reservation ID"], element["Status"]))
         sql.write('\n')
         for element in result['CUSTOMER_RESERVATION']:
-            sql.write("INSERT INTO Customer_Reservation VALUES ('{}','{}')\n".format(element["Customer Email"], element["Reservation ID"]))
-=======
-
-def theSpliter(roughList):
-    categoryCol = {}
-    category = ''
-    workSet = None
-    flag = False
-
-    for eachLine in roughList:
-        # Moving onto next category
-        if eachLine.startswith("---"):
-            if flag:
-                categoryCol[category].remove(workSet)
-                flag = False
-
-            category = eachLine.translate(None, ' -').strip()
-            categoryCol[category] = []
-
-            workSet = {}
-            categoryCol[category].append(workSet)
-
-        # Moving onto next element in the same category
-        elif eachLine == '\n':
-
-            workSet = {}
-            categoryCol[category].append(workSet)
-            flag = True
-
-        else:
-            attr = eachLine[:eachLine.find(':')]
-            content = eachLine[eachLine.find(':'):].strip(": ").strip()
-
-            workSet[attr] = content
-
-    return categoryCol
-
-
-if __name__ == "__main__":
-    result = {}
-
-    with open("Car-data (1).txt","r") as fd:
-        lines = fd.readlines()
-
-        result = theSpliter(roughList=lines)
-
-    with open("data.sql", "w") as sql:
-        # Insert into Customer
-        for element in result['Customer']:
-            sql.write("INSERT INTO Customer VALUES ('{}','{}','{}');\n".format(element["Name"], element["Age"], element["Email"]))
-        sql.write('\n')
-        for element in result['Model']:
-            sql.write("INSERT INTO Model VALUES ('{}','{}','{}','{}','{}');\n".format(element["ID"], element["Name"], element["Vehicle Type"], element["Model Number"], element["Capacity (Seats)"]))
-        sql.write('\n')
-        for element in result['RENTALSTATION']:
-            sql.write("INSERT INTO Rentalstation VALUES ('{}','{}','{}','{}','{}');\n".format(element["Station Code"], element["Name"], element["Address"], element["Area Code"], element["City"]))
-        sql.write('\n')
-        for element in result['CAR']:
-            sql.write("INSERT INTO Car VALUES ('{}','{}','{}','{}');\n".format(element["ID"], element["License Plate Number"], element["Station Code"], element["Model_Id"]))
-        sql.write('\n')
-        for element in result['RESERVATION']:
-            sql.write("INSERT INTO Reservation VALUES ('{}','{}','{}','{}','{}','{}');\n".format(element["ID"], element["From Date"], element["To Date"], element["Car ID"], element["Old Reservation ID"], element["Status"]))
-        sql.write('\n')
-        for element in result['CUSTOMER_RESERVATION']:
-            sql.write("INSERT INTO Customer_Reservation VALUES ('{}','{}');\n".format(element["Customer Email"], element["Reservation ID"]))
->>>>>>> 0b1c606362171e59e793f089672fb2b86e91cf85
+            sql.write("INSERT INTO Customer_Reservation VALUES ('{}',{});\n".format(element["Customer Email"], element["Reservation ID"]))
